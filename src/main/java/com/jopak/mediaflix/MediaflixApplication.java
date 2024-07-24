dbb2e0a79b2df181c934feb76a3b9d91be84c6c5
jopak19 <jpaulogomessiqueira@gmail.com>
2024-07-24 14:16:51 -0300

    projeto inicial spring

---
+++ src/main/java/com/jopak/mediaflix/MediaflixApplication.java
@@ -0,0 +1,13 @@
+package com.jopak.mediaflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jopak/mediaflix/MediaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jopak.mediaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
